fbcc7158a49f8008bc1382e7804e92572628a534
jh3y <jh3y@users.noreply.github.com>
2022-07-13 04:10:38 +0100

    feat: check in JavaScript comment for has transform form

--- src/has-transform-form/script.js
+++ src/has-transform-form/script.js
@@ -1,1 +1,3 @@
-console.info({ hello: 'world' });
+// Use JavaScript to progressively enhance the form and make it so "Enter"
+// progresses and autofocuses the next input
+console.info({ hello: 'progressive enhancement?' });
