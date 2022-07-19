cb35a67b2f650004294fba5ac6b024258e72f978
jh3y <jh3y@users.noreply.github.com>
2022-07-19 20:29:00 +0100

    feat: check in :has + popup demos

---
+++ src/openui-initiative/popup/popup-nav-drawer/script.js
@@ -0,0 +1,8 @@
+const NAV_CONTROL = document.querySelector('button')
+
+const CONTROL_NAV = () => {
+	NAV_CONTROL.setAttribute('aria-expanded', NAV_CONTROL.matches('[aria-expanded="false"]') ? true : false)
+	NAV_CONTROL.setAttribute('aria-pressed', NAV_CONTROL.matches('[aria-expanded="false"]') ? true : false)
+}
+
+NAV_CONTROL.addEventListener('click', CONTROL_NAV)
