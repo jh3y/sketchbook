1819b59f44a02f815ab642359f24dedf8db52d43
jh3y <jh3y@users.noreply.github.com>
2022-07-28 20:50:56 -0700

    feat: 3D pop up

---
+++ net/utils/range.js
@@ -0,0 +1,7 @@
+const mapRange = (inputLower, inputUpper, outputLower, outputUpper) => {
+  const INPUT_RANGE = inputUpper - inputLower
+  const OUTPUT_RANGE = outputUpper - outputLower
+  return (value) =>
+    outputLower + (((value - inputLower) / INPUT_RANGE) * OUTPUT_RANGE || 0)
+}
+const clamp = (min, max, value) => Math.min(Math.max(value, min), max)
--- src/openui-initiative/popup/3d-pop/script.js
+++ src/openui-initiative/popup/3d-pop/script.js
@@ -1,1 +1,29 @@
-console.info({ hello: 'world' });
+const mapRange = (inputLower, inputUpper, outputLower, outputUpper) => {
+  const INPUT_RANGE = inputUpper - inputLower
+  const OUTPUT_RANGE = outputUpper - outputLower
+  return (value) =>
+    outputLower + (((value - inputLower) / INPUT_RANGE) * OUTPUT_RANGE || 0)
+}
+const clamp = (min, max, value) => Math.min(Math.max(value, min), max)
+
+const POPUP = document.querySelector('.popup')
+
+const LIMIT = 10
+
+const UPDATE_PARALLAX = ({ x, y }) => {
+	const rotateY = clamp(-LIMIT, LIMIT, mapRange(0, window.innerWidth, -LIMIT, LIMIT)(y))
+	const rotateX = clamp(-LIMIT, LIMIT, mapRange(0, window.innerHeight, -LIMIT, LIMIT)(x)) 
+	document.documentElement.style.setProperty('--x', rotateX)
+	document.documentElement.style.setProperty('--y', rotateY)
+}
+
+const DEACTIVATE_PARALLAX = () => {
+	document.body.removeEventListener('pointermove', UPDATE_PARALLAX)
+}
+
+const ACTIVATE_PARALLAX = () => {
+	document.body.addEventListener('pointermove', UPDATE_PARALLAX)
+}
+
+POPUP.addEventListener('show', ACTIVATE_PARALLAX)
+POPUP.addEventListener('hide', DEACTIVATE_PARALLAX)
