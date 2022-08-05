5e9f3ffc727ef75bcd173125f205bfe88861a9fe
jh3y <jh3y@users.noreply.github.com>
2022-08-04 21:12:13 -0700

    feat: foundation for power input

--- net/utils/random-ranges.js
+++ net/utils/random-ranges.js
@@ -4,4 +4,10 @@ const mapRange = (inputLower, inputUpper, outputLower, outputUpper) => {
   return (value) =>
     outputLower + (((value - inputLower) / INPUT_RANGE) * OUTPUT_RANGE || 0)
 }
+
 const clamp = (min, max, value) => Math.min(Math.max(value, min), max)
+
+const randomInRange = (min, max) =>
+  Math.floor(
+    Math.random() * (Math.floor(max) - Math.ceil(min) + 1) + Math.ceil(min)
+  )
