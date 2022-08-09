b44d701dd23170063a4acabd7a769287aa0498d8
jh3y <jh3y@users.noreply.github.com>
2022-08-09 14:50:02 -0700

    feat: bouncy hamburger for speedy

---
+++ src/speedy-tips/bouncy-hamburger/script.js
@@ -0,0 +1,5 @@
+const BUTTON = document.querySelector('button')
+
+const TOGGLE = () => BUTTON.setAttribute('aria-pressed', BUTTON.matches('[aria-pressed="true"]') ? false : true)
+
+BUTTON.addEventListener('click', TOGGLE)
