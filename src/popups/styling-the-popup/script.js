15cb0982344a63f0cbbef925a1d9ed7936faae99
jh3y <jh3y@users.noreply.github.com>
2022-08-09 22:20:44 -0700

    feat: update popups collection

---
+++ src/popups/styling-the-popup/script.js
@@ -0,0 +1,6 @@
+const POPUP = document.querySelector('[popup]')
+
+const LOG = e => console.info({ event: e, type: e.type })
+
+POPUP.addEventListener('show', LOG)
+POPUP.addEventListener('hide', LOG)
