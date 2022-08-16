331f66c0e9d376c04dd3bc8b299da588755b1e32
jh3y <jh3y@users.noreply.github.com>
2022-08-16 14:25:41 -0700

    feat: check in pop-up stuff

---
+++ src/dcc/pop-ups/audio-pop-up/script.js
@@ -0,0 +1,42 @@
+const AUDIO_POP = new Audio(new URL('./pop.mp3', import.meta.url))
+const WORD = 'Pop-up'
+const START_INDEX = WORD.length / -2 + 0.5
+
+for (let p = 0; p < WORD.length; p++) {
+  const POPUP = Object.assign(document.createElement('button'), {
+    popUp: 'manual',
+    className: 'balloon',
+    id: WORD.charAt(p),
+    defaultOpen: true,
+    title: `Pop "${WORD.charAt(p)}"`,
+    innerHTML: `
+      <span class="balloon__content">
+        <span class="balloon__letter">${WORD.charAt(p)}</span>
+        <span class="balloon__handle"></div>
+      </span>
+    `,
+    style: `
+      --index: ${START_INDEX + p};
+      --hue: ${Math.random() * 359};
+      --bob-speed: ${Math.random() + 0.5};
+      --float-speed: ${Math.random() + 0.5};
+    `,
+  })
+  document.body.appendChild(POPUP)
+  POPUP.addEventListener('click', () => {
+    AUDIO_POP.currentTime = 0
+    AUDIO_POP.play()
+    POPUP.hidePopUp()
+    Object.assign(POPUP, {
+      style: `
+      --index: ${START_INDEX + p};
+      --hue: ${Math.random() * 359};
+      --bob-speed: ${Math.random() + 0.5};
+      --float-speed: ${Math.random() + 0.5};
+    `,
+    })
+    requestAnimationFrame(() => POPUP.showPopUp())
+  })
+  // Because defaultopen does not work for some reason?
+  POPUP.showPopUp()
+}
--- src/experimental-css-primitives/scroll-timeline/sandbox/script.js
+++ src/experimental-css-primitives/scroll-timeline/sandbox/script.js
@@ -10,3 +10,101 @@ const STYLES_TO_INSERT = `
 `
 
 insertStyles(STYLES_TO_INSERT)
+
+var elem = document.getElementById("container");
+var st = new ScrollTimeline();
+var a = document
+  .querySelector("#test")
+  .animate({
+    rotate: ['0deg', '360deg', '360deg'],
+    offset: [0, 0.8, 1]
+  },
+    // [
+    //   { rotate: '0deg', offset: 0 },
+    //   { rotate: '360deg', offset: [0.8, 1] }
+    // ],
+    {
+      // duration: 50,
+      // rotate: ['0deg', '360deg'],
+      fillMode: 'forwards',
+      timeline: st
+    }
+  );
+
+
+
+// Use the scroll-timeline polyfills
+
+  "use strict";
+
+  const progressBars = document.querySelectorAll('.progress-bar-progress');
+  const createProgressAnimation = (bar, range, orientation) => {
+    const target = document.getElementById('target');
+    const viewTimeline = new ViewTimeline({
+      'subject': target,
+      'axis': orientation
+    });
+    bar.animate( { width: ['0px', '200px' ] }, {
+      timeline: viewTimeline,
+      timeRange: `${range}`,
+      fill: 'both'
+    });
+  }
+  const createAnimations = (selection) => {
+    const orientation = selection == 'vertical-tb' ? 'block' : 'inline';
+    document.getAnimations().forEach(anim => {
+      anim.cancel();
+    });
+    createProgressAnimation(progressBars[0], 'cover', orientation);
+    createProgressAnimation(progressBars[1], 'contain', orientation);
+    createProgressAnimation(progressBars[2], 'enter', orientation);
+    createProgressAnimation(progressBars[3], 'exit', orientation);
+    createProgressAnimation(progressBars[4], 'contain 25% 75%', orientation);
+    createProgressAnimation(progressBars[5], 'enter 150% exit -50%',
+                            orientation);
+  };
+
+  document.querySelectorAll('input').forEach(input => {
+    input.addEventListener('change', (evt) => {
+      document.getAnimations().forEach(anim => {
+        anim.cancel();
+      });
+      const selection = event.target.id;
+      switch(selection) {
+        case 'vertical-tb':
+          container.classList.remove('rtl');
+          container.classList.remove('ltr');
+          overlay.classList.remove('rtl');
+          break;
+
+        case 'horizontal-ltr':
+          container.classList.remove('rtl');
+          container.classList.add('ltr');
+          overlay.classList.remove('rtl');
+          break;
+
+        case 'horizontal-rtl':
+          container.classList.add('rtl');
+          container.classList.remove('ltr');
+          overlay.classList.add('rtl');
+      }
+      createAnimations(evt.target.id);
+    });
+  });
+
+  createAnimations('vertical-tb');
+
+var elem = document.getElementById("container");
+var st = new ScrollTimeline();
+var a = document
+  .querySelector("#test")
+  .animate(
+    [
+      { rotate: '0deg' },
+      { rotate: '360deg' }
+    ],
+    {
+      // duration: 50,
+      timeline: st
+    }
+  );
