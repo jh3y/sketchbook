66fb9468019d40e101b7a4ff2c4d90c24ed9f1a9
jh3y <jh3y@users.noreply.github.com>
2023-03-22 00:27:16 +0000

    feat: start generator button

---
+++ src/particle-generator-button/script.js
@@ -0,0 +1,20 @@
+// import gsap from 'gsap'
+
+// const duration = 4
+// gsap.defaults({
+// 	ease: 'power1.easeInOut'
+// })
+
+// gsap.timeline({ repeat: -1, repeatDelay: 1 })
+// 	.set('button', {
+// 		'--tail': 0,
+// 		'--head': 0,
+// 	})
+// 	.to('button', {
+// 		'--head': 360,
+// 		duration
+// 	})
+// 	.to('button', {
+// 		'--tail': 360,
+// 		duration,
+// 	}, duration * 0)
