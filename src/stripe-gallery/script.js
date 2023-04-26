80d1d863b8b5d2948ee2413866b69bbb34ce215f
jh3y <jh3y@users.noreply.github.com>
2023-04-26 14:13:53 +0100

    feat: check in stripe gallery example

--- src/stripe-gallery/script.js
+++ src/stripe-gallery/script.js
@@ -1,11 +1,16 @@
 import { faker } from '@faker-js/faker'
 
-const SPEAKERS = document.querySelectorAll('.speaker')
+const SPEAKERS = document.querySelectorAll('li')
 SPEAKERS.forEach((SPEAKER, index) => {
   const img = SPEAKER.querySelector('img')
+  const details = SPEAKER.querySelector('.speaker__details')
   const name = SPEAKER.querySelector('.speaker__name')
   const title = SPEAKER.querySelector('.speaker__title')
-  name.innerText = `${faker.name.firstName()} ${faker.name.lastName()}`
-  title.innerText = `${faker.name.jobTitle()}, ${faker.company.name()}`
+  const fullName = `${faker.name.firstName()} ${faker.name.lastName()}`
+  const job = `${faker.name.jobTitle()}, ${faker.company.name()}`
+  name.innerText = fullName
+  title.innerText = job
+  details.innerText = `${fullName}. ${job}`
   img.src = `https://assets.codepen.io/605876/headshot-${index + 1}.png?format=auto`
+  img.alt = `Image of ${fullName}`
 })
