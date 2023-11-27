136563b6a90ec70bb344e1021e2e1e3c093a302b
jh3y <jh3y@users.noreply.github.com>
2023-11-27 21:58:45 +0000

    feat: messing around with window trail and animation

--- src/react-multi-window/script.jsx
+++ src/react-multi-window/script.jsx
@@ -6,17 +6,27 @@ import ReactDOM from 'https://cdn.skypack.dev/react-dom'
 const ROOT_NODE = document.querySelector('#app')
 const STORAGE_KEY = 'windows'
 
+// Utility functions
+const readFromStorage = () =>
+  JSON.parse(window.localStorage.getItem(STORAGE_KEY)) || {}
+
+const writeToStorage = (data) =>
+  window.localStorage.setItem(STORAGE_KEY, JSON.stringify({ ...data }))
+
+const openWindow = ({ height, name, width, top, left, query }) => {
+  window.open(
+    `${window.location.origin}${query || ''}`,
+    name || '_blank',
+    `top=${top || 0},left=${left || 0},height=${height || 300},width=${
+      width || 300
+    }`
+  )
+}
 
 const useWindow = (collectionName) => {
-  const readFromStorage = () =>
-    JSON.parse(window.localStorage.getItem(STORAGE_KEY)) || {}
-  const writeToStorage = (data) =>
-    window.localStorage.setItem(STORAGE_KEY, JSON.stringify({ ...data }))
   const [windows, setWindows] = React.useState(readFromStorage)
   const [id, setId] = React.useState(crypto.randomUUID())
-  const [primary, setPrimary] = React.useState(
-    Object.keys(windows).length === 0
-  )
+  const [index, setIndex] = React.useState(Object.keys(windows).length)
   const [meta, setMeta] = React.useState({
     left: screenLeft,
     top: screenTop,
@@ -31,7 +41,7 @@ const useWindow = (collectionName) => {
       ...windows,
       [id]: {
         id,
-        primary,
+        index,
         meta,
       },
     }))
@@ -60,10 +70,10 @@ const useWindow = (collectionName) => {
   React.useEffect(() => {
     const handleDrop = () => {
       gsap.ticker.remove(sync)
-      const current = {...windows}
+      const current = { ...windows }
       delete current[id]
       writeToStorage({})
-      if (primary) {
+      if (index === 0) {
         broadcastChannel.current.postMessage({ kill: true })
       }
     }
@@ -87,48 +97,151 @@ const useWindow = (collectionName) => {
     writeToStorage({
       ...current,
       [id]: {
-        ...meta,
         id,
-        primary,
+        index,
+        ...meta,
       },
     })
   }, [meta, windows])
 
-  // const resize = (width = outerWidth, height = outerHeight) => {
-  //   if (primary) console.warn("You can't resize the primary window unless opened by another window")
-  //   const proxy = {
-  //     width: outerWidth, height: outerHeight
-  //   }
-  //   gsap.to(proxy, {
-  //     width,
-  //     height,
-  //     onUpdate: function() {
-  //       window.resizeTo(proxy.width, proxy.height)
-  //     }
-  //   })
-  // }
+  const resize = (width = outerWidth, height = outerHeight, options) => {
+    if (index === 0)
+      console.warn(
+        "You can't resize the primary window unless opened by another window"
+      )
+    const proxy = {
+      width: outerWidth,
+      height: outerHeight,
+    }
+    gsap.to(proxy, {
+      width,
+      height,
+      ...options,
+      onUpdate: function () {
+        window.resizeTo(proxy.width, proxy.height)
+      },
+    })
+  }
+
+  const move = (x = screenLeft, y = screenTop, options) => {
+    if (index === 0)
+      console.warn(
+        "You can't move the primary window unless opened by another window"
+      )
+    const proxy = {
+      x: screenLeft,
+      y: screenTop,
+    }
+    gsap.to(proxy, {
+      x,
+      y,
+      ...options,
+      onUpdate: function () {
+        window.moveTo(proxy.x, proxy.y)
+      },
+    })
+  }
 
   return {
     broadcastChannel: broadcastChannel.current,
     id,
+    index,
     ...meta,
-    primary,
-    // resize,
+    move,
+    resize,
   }
 }
 
 const App = () => {
-  const { id, left } = useWindow('multi-window')
+  const { height, id, index, left, move, resize, top, width } =
+    useWindow('multi-window')
 
-  const openSecondary = () => {
-    window.open(window.location.href, '_blank', 'height=600,width=600')
+  React.useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search)
+    const random = urlParams.get('random')
+    // If the random queryString is set and the index isn't the primary one...
+    if (index !== 0 && parseInt(random, 10) === 1) {
+      const smin = Math.min(screen.availHeight, screen.availHeight)
+      const size = gsap.utils.random(smin * 0.2, smin * 0.8, 1)
+      const x = gsap.utils.random(0, screen.availWidth - size, 1)
+      const y = gsap.utils.random(0, screen.availHeight - size, 1)
+      resize(size, size, {
+        ease: 'bounce.out',
+        delay: 0.5,
+        onComplete: () =>
+          move(x, y, { ease: 'elastic.out', delay: 0.5, duration: 1 }),
+      })
+    }
+  }, [])
+
+  const openSingleWindow = () => {
+    openWindow({
+      height: 300,
+      width: 300,
+      top: 200,
+      left: 200,
+      query: '?random=1',
+    })
+  }
+
+  const openWindowTrail = () => {
+    const newWindows = new Array(10).fill({ x: 0 })
+    const smin = Math.min(screen.availHeight, screen.availHeight)
+    const yDist = gsap.utils.distribute({
+      base: 0,
+      amount: screen.availHeight - smin,
+      from: 0,
+      ease: 'power1.in',
+    })
+    const xDist = gsap.utils.distribute({
+      base: 0,
+      amount: screen.availWidth * 0.5 - smin,
+      from: 0,
+      ease: 'power1.in',
+    })
+
+    const tl = gsap.timeline()
+
+    for (let i = 0; i < newWindows.length; i++) {
+      setTimeout(() => {
+        console.info('cool', window.location.origin)
+        window.open(window.location.origin, `window--${i}`, 'height=300,width=300,popup=true')
+      }, i * 1000)
+      // const newWindow = newWindows[i]
+      // tl.to(newWindow, {
+      //   x: 100,
+      //   duration: 0.5,
+      //   onStart: function () {
+      //     console.info('cool')
+      //     openWindow({
+      //       height: 300,
+      //       width: 300,
+      //       left: xDist(i, newWindow[i], newWindows),
+      //       top: yDist(i, newWindow[i], newWindows)
+      //     })
+      //   },
+      // })
+    }
   }
 
   return (
     <main>
-      <h1>{`Howdy: ${id}`}</h1>
-      <button onClick={openSecondary}>Open Secondary</button>
-      <span>{`left: ${left}`}</span>
+      <dl>
+        <dt>id:</dt>
+        <dd>{id}</dd>
+        <dt>index:</dt>
+        <dd>{index}</dd>
+        <dt>top:</dt>
+        <dd>{top}</dd>
+        <dt>left:</dt>
+        <dd>{left}</dd>
+        <dt>width:</dt>
+        <dd>{width}</dd>
+        <dt>height:</dt>
+        <dd>{height}</dd>
+      </dl>
+      <button onClick={openSingleWindow}>Open Window</button>
+      <button onClick={openWindowTrail}>Open Window Trail</button>
     </main>
   )
 }
