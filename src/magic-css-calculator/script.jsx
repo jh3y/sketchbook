7f46b971d94fcf797e815646ba04c8940ecbb583
jh3y <jh3y@users.noreply.github.com>
2024-01-23 01:54:52 -0700

    feat: functional rudimentary has calculator

--- src/magic-css-calculator/script.jsx
+++ src/magic-css-calculator/script.jsx
@@ -4,43 +4,40 @@ import { render } from 'https://cdn.skypack.dev/react-dom'
 const ROOT_NODE = document.querySelector('#app')
 
 const App = () => {
-  const moves = 4
+  const moves = 8
   return (
     <form className="calculator">
       {new Array(moves).fill(0).map((_, index) => (
         <div className="calculator__move" data-move={index}>
           {new Array(10).fill(0).map((_, digit) => (
             <>
-              <input id={`d-${index}-${digit}`} type="checkbox" className="calculator__digit" />
+              <input id={`d-${index}-${digit}`} type="radio" data-symbol={digit} name={`d-${index}`} />
               <label for={`d-${index}-${digit}`}>{digit}</label>   
             </>
           ))}
         </div>
       ))}
       <div className="calculator__operands">
-        <input id="o-plus" type="checkbox" className="calculator__operand" />
-        <label for="o-plus">+</label>   
-        <input id="o-minus" type="checkbox" className="calculator__operand" />
-        <label for="o-minus">-</label>   
-        <input id="o-multiply" type="checkbox" className="calculator__operand" />
+        <input id="o-divide" type="radio" name="operand" />
+        <label for="o-divide">÷</label>
+        <input id="o-multiply" type="radio" name="operand" />
         <label for="o-multiply">×</label>   
-        <input id="o-divide" type="checkbox" className="calculator__operand" />
-        <label for="o-divide">÷</label>   
+        <input id="o-minus" type="radio" name="operand" />
+        <label for="o-minus">-</label>   
+        <input id="o-plus" type="radio" name="operand" />
+        <label for="o-plus">+</label>
       </div>
-      {new Array(moves).fill(0).map((_, index) => (
-        <div className="calculator__move" data-move={moves + index}>
-          {new Array(10).fill(0).map((_, digit) => (
-            <>
-              <input id={`d-${moves + index}-${digit}`} type="checkbox" className="calculator__digit" />
-              <label for={`d-${moves + index}-${digit}`}>{digit}</label>   
-            </>
-          ))}
-        </div>
-      ))}
-      <div className="first"></div>  
-      <div className="second"></div>
-      <div className="output"></div>
-      <button type="reset">Reset</button>    
+      <div className="calculator__calculate">
+        <input type="checkbox" id="calculate" />
+        <label for="calculate">=</label>
+      </div>   
+      <div className="results">
+        <div className="first"></div>  
+        <div className="second"></div>
+        <div className="output"></div>
+        <div className="final"></div>
+      </div>
+      <button type="reset">C</button>
     </form>
   )
 }
