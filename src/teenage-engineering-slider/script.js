ce8388c39ba0c78b222b5798ce0d0628b554aaad
jh3y <jh3y@users.noreply.github.com>
2024-01-28 18:04:17 -0700

    feat: te styled range slider

---
+++ src/teenage-engineering-slider/script.js
@@ -0,0 +1,25 @@
+import { Pane } from 'https://cdn.skypack.dev/tweakpane'
+
+
+if (!CSS.supports('animation-timeline: scroll()')) {
+  const CONTROL = document.querySelector('.control')
+  const TRACK = CONTROL.querySelector('.control__track')
+  const INPUT = document.querySelector('input')
+  const update = () => {
+    CONTROL.style.setProperty('--value', INPUT.value)
+  }
+  INPUT.addEventListener('input', update)
+  INPUT.addEventListener('pointerdown', update)
+  update()
+}
+
+const CONFIG = {
+  hue: 164,
+}
+const update = () => {
+  document.documentElement.style.setProperty('--hue', CONFIG.hue)
+}
+const CTRL = new Pane()
+CTRL.addBinding(CONFIG, 'hue', { min: 0, max: 359, step: 1 })
+CTRL.on('change', update)
+update()
