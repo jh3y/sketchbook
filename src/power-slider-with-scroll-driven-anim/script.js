4fe02b2550f112162cbe8e21e32ba9e903acd1c2
jh3y <jh3y@users.noreply.github.com>
2024-01-28 18:04:01 -0700

    feat: fix scroll intersectionobserver code

--- src/power-slider-with-scroll-driven-anim/script.js
+++ src/power-slider-with-scroll-driven-anim/script.js
@@ -1,3 +1,6 @@
+import { Pane } from 'https://cdn.skypack.dev/tweakpane'
+
+
 if (!CSS.supports('animation-timeline: scroll()')) {
   const CONTROL = document.querySelector('.control')
   const TRACK = CONTROL.querySelector('.control__track')
@@ -9,3 +12,14 @@ if (!CSS.supports('animation-timeline: scroll()')) {
   INPUT.addEventListener('pointerdown', update)
   update()
 }
+
+const CONFIG = {
+  hue: 30
+}
+const update = () => {
+  document.documentElement.style.setProperty('--hue', CONFIG.hue)
+}
+const CTRL = new Pane()
+CTRL.addBinding(CONFIG, 'hue', { min: 0, max: 359, step: 1 })
+CTRL.on('change', update)
+
--- src/scroll-highlighting/script.js
+++ src/scroll-highlighting/script.js
@@ -14,3 +14,24 @@ const TOGGLE_THEME = () => {
 }
 
 TOGGLE.addEventListener('click', TOGGLE_THEME)
+
+if (!CSS.supports('animation-timeline: scroll()')) {
+  console.info('backfill with IntersectionObserver')
+  const marks = document.querySelectorAll('mark')
+
+  const onObserve = (entries) => {
+    // console.info(entries)
+    entries.forEach(entry => {
+      entry.target.style.setProperty('--highlighted', entry.isIntersecting ? 1 : 0)
+    })
+  }
+
+  const highlightObserver = new IntersectionObserver(onObserve, {
+    rootMargin: 0,
+    threshold: 0
+  })
+
+  console.info({ marks })
+
+  marks.forEach(mark => highlightObserver.observe(mark))
+}
