c30a165e0f83b82fa800047febe6d89117a498cc
jh3y <jh3y@users.noreply.github.com>
2024-02-01 22:37:04 +0000

    feat: tear strip finish

--- src/css-tear-strip/script.js
+++ src/css-tear-strip/script.js
@@ -1,3 +1,5 @@
-const input = document.querySelector('input')
-const update = () => document.documentElement.style.setProperty('--value', input.value)
-input.addEventListener('input', update)
+if (!CSS.supports('animation-timeline: scroll()')) {
+  const input = document.querySelector('input')
+  const update = () => document.documentElement.style.setProperty('--value', input.value)
+  input.addEventListener('input', update)
+}
