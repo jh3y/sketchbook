a006976a978387032702a0e6b6207d94b96641a2
jh3y <jh3y@users.noreply.github.com>
2024-02-07 00:41:07 +0000

    feat: matrix button updates

--- src/matrix-upload-button/script.js
+++ src/matrix-upload-button/script.js
@@ -111,7 +111,7 @@ DigitalRain.prototype.init = function () {
 DigitalRain.prototype.getColor = function (
   x,
   y,
-  { hue, row, len, lastLen, lastDestination, tailCounter }
+  { active, hue, row, len, lastLen, lastDestination, tailCounter }
 ) {
   const self = this
   // If y > row but less than last destination, work out the color as if row === column.lastDestination
@@ -135,7 +135,7 @@ DigitalRain.prototype.getColor = function (
     alpha = lower
   }
 
-  if (self.isCellLabel({ x, y })) {
+  if (self.isCellLabel({ x, y }) && !active) {
     return 'white'
   }
 
@@ -149,7 +149,7 @@ DigitalRain.prototype.isCellLabel = function ({ x, y }) {
   const mid = Math.floor((self.columns - 1) * 0.5)
   const low = mid - self.options.word.length * 0.5
   const high = mid + self.options.word.length * 0.5
-  if (x > low && x <= high && y === Math.round(self.rows * 0.5)) {
+  if (x > low && x <= high && y === Math.round(self.rows * 0.5) - 1) {
     return true
   }
   return false
@@ -164,9 +164,7 @@ DigitalRain.prototype.render = function () {
     const y = Math.floor(c / self.columns)
     const column = self.tracker[x]
 
-    const active = false
-
-    if (!active && !self.isCellLabel({ x, y })) continue
+    if (!self.active && !self.isCellLabel({ x, y })) continue
 
     // On the first row, let's bump the index
     if (y === 0 && Math.random() > 0.1) {
@@ -201,14 +199,21 @@ DigitalRain.prototype.render = function () {
       // 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
       // [5, 6, 7, 8, 9, 10]
 
+      // Gotta work out what happens here.
+      // Go active then it can be scrubbed.
+
       let char = chars[y]
-      if (self.isCellLabel({ x, y })) {
+      if (self.isCellLabel({ x, y }) && !column.active) {
         char =
           self.options.word.split('')[
             x -
               1 -
               (Math.floor((self.columns - 1) * 0.5) - self.options.word.length * 0.5)
           ]
+        if (self.active && column.row === 8) {
+          column.active = true
+          // char = 'X'
+        }
       }
 
       self.context.fillText(
@@ -225,19 +230,24 @@ DigitalRain.prototype.render = function () {
   }
 }
 
+DigitalRain.prototype.activate = function () {
+  const self = this
+  self.active = true
+}
+
 // Not gonna need this as it's for resetting the sizes
 DigitalRain.prototype.setSize = function () {
   const self = this
   const { height, width } = self.canvas.getBoundingClientRect()
-  self.canvas.height = height * self.__ratio
-  self.canvas.width = width * self.__ratio
+  self.canvas.height = height * 0.5 * self.__ratio
+  self.canvas.width = width * 0.5 * self.__ratio
   // Set the font size and get the rows/columns
   self.fontSize = Math.ceil(
     typeof self.size === 'function' ? self.size() : self.size
   )
   self.columns = Math.ceil(self.canvas.width / self.fontSize)
   // self.columns = 1
-  self.rows = Math.ceil(self.canvas.height / self.fontSize)
+  self.rows = Math.ceil(self.canvas.height / self.fontSize) + 1
   self.characters = self.rows * self.columns
   self.context.font = `${self.fontSize}px ${self.options.family}`
   self.context.textAlign = 'center'
@@ -245,13 +255,15 @@ DigitalRain.prototype.setSize = function () {
 
 window.myDigitalRain = new DigitalRain(CANVAS, {
   ...DEFAULT_OPTIONS,
-  word: 'UPLOAD',
+  word: 'DOWNLOAD',
   // Start word and end word can switch between them
 })
 
 const BUTTON = document.querySelector('button')
-const UPLOAD = () => {}
-BUTTON.addEventListener('click', UPLOAD)
+const DOWNLOAD = () => {
+  myDigitalRain.activate()
+}
+BUTTON.addEventListener('click', DOWNLOAD)
 
 
 // Perhaps the best thing to do is overlay one canvas on the other
