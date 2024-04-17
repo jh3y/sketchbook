6585db89e312258153b5ebc064c39dd9134c0afe
jh3y <jh3y@users.noreply.github.com>
2024-04-17 20:44:32 +0100

    feat: check in osaka card

--- src/osaka-blur-card/script.js
+++ src/osaka-blur-card/script.js
@@ -1,1 +1,10 @@
-console.info({ hello: 'world' });
+import gsap from 'https://cdn.skypack.dev/gsap@3.12.0'
+
+const UPDATE = ({ x, y }) => {
+  gsap.set(document.documentElement, {
+    '--x': gsap.utils.mapRange(0, window.innerWidth, -1, 1, x),
+    '--y': gsap.utils.mapRange(0, window.innerHeight, -1, 1, y),
+  })
+}
+
+window.addEventListener('pointermove', UPDATE)
