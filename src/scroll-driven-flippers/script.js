7c44f8cb670d61a234d3ab45c7e0604f8ca17baa
jh3y <jh3y@users.noreply.github.com>
2024-04-26 22:16:04 +0100

    feat: check in flippers

--- src/scroll-driven-flippers/script.js
+++ src/scroll-driven-flippers/script.js
@@ -1,1 +1,16 @@
-console.info({ hello: 'world' });
+import { Pane } from 'https://cdn.skypack.dev/tweakpane'
+
+const CONFIG = {
+  guides: false
+}
+
+const CTRL = new Pane({ title: 'Configuration', expanded: false })
+CTRL.addBinding(CONFIG, 'guides', { label: 'Show Guides'})
+
+const UPDATE = () => {
+  document.documentElement.dataset.guides = CONFIG.guides
+}
+
+CTRL.on('change', UPDATE)
+
+UPDATE()
