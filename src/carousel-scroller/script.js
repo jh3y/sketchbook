3f803821720bd4a482accacda7b44d11246e5c11
jh3y <jh3y@users.noreply.github.com>
2024-05-08 01:23:06 +0100

    feat: pre config carousel

--- src/carousel-scroller/script.js
+++ src/carousel-scroller/script.js
@@ -1,31 +1,79 @@
 import { Pane } from 'https://cdn.skypack.dev/tweakpane'
 
 const CONFIG = {
-  debug: false,
-  spread: false,
+  debug: true,
+  spread: true,
   zoom: false,
-  rotate: false,
+  rotate: true,
   animate: false,
-  scroll: false,
+  scroll: true,
+  dark: false,
   items: 14,
   gap: 0.1,
 }
 
 const MAIN = document.querySelector('main')
 
-const render = () => {
-  MAIN.innerHTML = `
-    <ul class="carousel" style="--total: ${CONFIG.items};">
-      ${new Array(CONFIG.items).fill().map((_, i) => (
-        `<li style="--index: ${i};">
+const generateItems = () => {
+  const items = []
+  const controllers = []
+  const scopes = ['--controller']
+
+  for (let i = 0; i < CONFIG.items + 1; i++) {
+    scopes.push(`--item-${i}`)
+    if (i !== CONFIG.items) {
+      items.push(`
+        <li style="--index: ${i}; --scoped: ${i === 0 ? `--item-${CONFIG.items},` : ''} --item-${i};">
           <img src="https://picsum.photos/300/300?random=${i}" alt="" />
-        </li>`
-      )).join('')}
-    </ul>
-  `
+        </li>
+      `)
+    }
+    controllers.push(`<li style="view-timeline: --item-${i} inline;"></li>`)
+  }
+
+  return {
+    items: items.join(''),
+    controllers: controllers.join(''),
+    scopes: `${scopes.join(',')};`,
+  }
 }
 
-const sync = () => {
+let scroller
+
+const handleScroll = () => {
+  if (scroller.scrollLeft + window.innerWidth > scroller.scrollWidth - 2) {
+    scroller.scrollLeft = 2
+  }
+  if (scroller.scrollLeft < 2) {
+    scroller.scrollLeft = scroller.scrollWidth - 2
+  }
+}
+
+const setupController = () => {
+  scroller = document.querySelector('.controller')
+  scroller.addEventListener('scroll', handleScroll)
+}
+
+const render = () => {
+  const { controllers, items, scopes } = generateItems()
+
+  MAIN.innerHTML = `
+    <div class="container" style="--total: ${CONFIG.items}; timeline-scope: ${scopes}">
+      <div class="carousel-container">
+        <ul class="carousel">
+          ${items}
+        </ul>
+      </div>
+      <ul class="controller" style="scroll-timeline: --controller inline;">
+        ${controllers}
+      </ul>
+    </div>
+  `
+
+  setupController()
+}
+
+const update = () => {
   console.info('changes')
   document.documentElement.dataset.debug = CONFIG.debug
   document.documentElement.dataset.spread = CONFIG.spread
@@ -33,7 +81,13 @@ const sync = () => {
   document.documentElement.dataset.rotate = CONFIG.rotate
   document.documentElement.dataset.animate = CONFIG.animate
   document.documentElement.dataset.scroll = CONFIG.scroll
+  document.documentElement.dataset.dark = CONFIG.dark
   document.documentElement.style.setProperty('--gap-efficient', CONFIG.gap)
+}
+
+const sync = () => {
+  if (!document.startViewTransition) return update()
+  document.startViewTransition(update)
 }
 
 const controlPanel = new Pane({ title: 'Config', expanded: true })
@@ -44,8 +98,19 @@ controlPanel.addBinding(CONFIG, 'rotate', { label: 'Rotate' })
 controlPanel.addBinding(CONFIG, 'animate', { label: 'Animate' })
 controlPanel.addBinding(CONFIG, 'scroll', { label: 'Scroll Drive' })
 controlPanel.addBinding(CONFIG, 'debug', { label: 'Debug' })
-controlPanel.addBinding(CONFIG, 'gap', { label: 'Gap (%)', min: -0.1, max: 5, step: 0.1 })
-const itemSwitch = controlPanel.addBinding(CONFIG, 'items', { label: 'Items', min: 10, max: 50, step: 1 })
+controlPanel.addBinding(CONFIG, 'dark', { label: 'Dark Theme' })
+controlPanel.addBinding(CONFIG, 'gap', {
+  label: 'Gap (%)',
+  min: -0.1,
+  max: 5,
+  step: 0.1,
+})
+const itemSwitch = controlPanel.addBinding(CONFIG, 'items', {
+  label: 'Items',
+  min: 10,
+  max: 50,
+  step: 1,
+})
 
 render()
 sync()
