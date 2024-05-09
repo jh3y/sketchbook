98c46901abf0abef24b15bfad132dac26a1b6721
jh3y <jh3y@users.noreply.github.com>
2024-05-09 13:32:24 +0100

    feat: check in carousel scroller

--- src/carousel-scroller/script.js
+++ src/carousel-scroller/script.js
@@ -1,15 +1,23 @@
 import { Pane } from 'https://cdn.skypack.dev/tweakpane'
 
 const CONFIG = {
-  debug: true,
-  spread: true,
-  zoom: false,
-  rotate: true,
-  animate: false,
+  debug: false,
+  // spread: false,
+  // zoom: false,
+  backface: false,
+  // rotate: false,
+  animate: true,
   scroll: true,
-  dark: false,
-  items: 14,
+  dark: true,
+  masklower: 30,
+  maskupper: 10,
+  perspective: 280,
+  vertical: false,
+  infinite: false,
+  items: 16,
   gap: 0.1,
+  rotatex: 0,
+  rotatez: 0,
 }
 
 const MAIN = document.querySelector('main')
@@ -17,35 +25,43 @@ const MAIN = document.querySelector('main')
 const generateItems = () => {
   const items = []
   const controllers = []
-  const scopes = ['--controller']
 
   for (let i = 0; i < CONFIG.items + 1; i++) {
-    scopes.push(`--item-${i}`)
+    // scopes.push(`--item-${i}`)
     if (i !== CONFIG.items) {
       items.push(`
-        <li style="--index: ${i}; --scoped: ${i === 0 ? `--item-${CONFIG.items},` : ''} --item-${i};">
+        <li style="--index: ${i};">
           <img src="https://picsum.photos/300/300?random=${i}" alt="" />
         </li>
       `)
     }
-    controllers.push(`<li style="view-timeline: --item-${i} inline;"></li>`)
+    controllers.push('<li></li>')
   }
 
   return {
     items: items.join(''),
     controllers: controllers.join(''),
-    scopes: `${scopes.join(',')};`,
   }
 }
 
 let scroller
 
 const handleScroll = () => {
-  if (scroller.scrollLeft + window.innerWidth > scroller.scrollWidth - 2) {
-    scroller.scrollLeft = 2
-  }
-  if (scroller.scrollLeft < 2) {
-    scroller.scrollLeft = scroller.scrollWidth - 2
+  if (!CONFIG.infinite) return false
+  if (CONFIG.vertical) {
+    if (scroller.scrollTop + window.innerHeight > scroller.scrollHeight - 2) {
+      scroller.scrollTop = 2
+    }
+    if (scroller.scrollTop < 2) {
+      scroller.scrollTop = scroller.scrollHeight - 2
+    }
+  } else {
+    if (scroller.scrollLeft + window.innerWidth > scroller.scrollWidth - 2) {
+      scroller.scrollLeft = 2
+    }
+    if (scroller.scrollLeft < 2) {
+      scroller.scrollLeft = scroller.scrollWidth - 2
+    }
   }
 }
 
@@ -55,53 +71,94 @@ const setupController = () => {
 }
 
 const render = () => {
-  const { controllers, items, scopes } = generateItems()
-
+  const { controllers, items } = generateItems()
   MAIN.innerHTML = `
-    <div class="container" style="--total: ${CONFIG.items}; timeline-scope: ${scopes}">
+    <div class="container" style="--total: ${CONFIG.items};">
       <div class="carousel-container">
         <ul class="carousel">
           ${items}
         </ul>
       </div>
-      <ul class="controller" style="scroll-timeline: --controller inline;">
+      <ul class="controller">
         ${controllers}
       </ul>
     </div>
   `
-
   setupController()
 }
 
 const update = () => {
-  console.info('changes')
   document.documentElement.dataset.debug = CONFIG.debug
-  document.documentElement.dataset.spread = CONFIG.spread
-  document.documentElement.dataset.zoom = CONFIG.zoom
-  document.documentElement.dataset.rotate = CONFIG.rotate
+  // document.documentElement.dataset.spread = CONFIG.spread
+  // document.documentElement.dataset.zoom = CONFIG.zoom
+  // document.documentElement.dataset.rotate = CONFIG.rotate
   document.documentElement.dataset.animate = CONFIG.animate
+  document.documentElement.dataset.backface = CONFIG.backface
   document.documentElement.dataset.scroll = CONFIG.scroll
   document.documentElement.dataset.dark = CONFIG.dark
+  document.documentElement.dataset.vertical = CONFIG.vertical
+  document.documentElement.dataset.infinite = CONFIG.infinite
   document.documentElement.style.setProperty('--gap-efficient', CONFIG.gap)
+  document.documentElement.style.setProperty('--rotate-x', CONFIG.rotatex)
+  document.documentElement.style.setProperty('--rotate-z', CONFIG.rotatez)
+  document.documentElement.style.setProperty('--mask-lower', CONFIG.masklower)
+  document.documentElement.style.setProperty('--mask-upper', CONFIG.maskupper)
+  document.documentElement.style.setProperty(
+    '--perspective',
+    CONFIG.perspective
+  )
 }
 
-const sync = () => {
-  if (!document.startViewTransition) return update()
+const sync = (event) => {
+  if (
+    !document.startViewTransition ||
+    !event ||
+    (event &&
+      event.target.controller.view.labelElement.innerText !== 'Dark Theme' &&
+      event.target.controller.view.labelElement.innerText !== 'Backface')
+  )
+    return update()
   document.startViewTransition(update)
 }
 
-const controlPanel = new Pane({ title: 'Config', expanded: true })
+const controlPanel = new Pane({ title: 'Config', expanded: false })
 
-controlPanel.addBinding(CONFIG, 'spread', { label: 'Spread' })
+// const spreader = controlPanel.addBinding(CONFIG, 'spread', { label: 'Spread' })
 // controlPanel.addBinding(CONFIG, 'zoom', { label: 'Zoom' })
-controlPanel.addBinding(CONFIG, 'rotate', { label: 'Rotate' })
+// controlPanel.addBinding(CONFIG, 'rotate', { label: 'Rotate' })
 controlPanel.addBinding(CONFIG, 'animate', { label: 'Animate' })
-controlPanel.addBinding(CONFIG, 'scroll', { label: 'Scroll Drive' })
-controlPanel.addBinding(CONFIG, 'debug', { label: 'Debug' })
-controlPanel.addBinding(CONFIG, 'dark', { label: 'Dark Theme' })
+const scrolling = controlPanel.addFolder({ title: 'Scrolling' , expanded: false })
+scrolling.addBinding(CONFIG, 'scroll', { label: 'Scroll Drive' })
+scrolling.addBinding(CONFIG, 'vertical', { label: 'Vertical' })
+scrolling.addBinding(CONFIG, 'infinite', { label: 'Infinite' })
+scrolling.addBinding(CONFIG, 'debug', { label: 'Debug' })
+const rotation = controlPanel.addFolder({ title: 'Rotation', expanded: false })
+rotation.addBinding(CONFIG, 'rotatex', { min: 0, max: 360, step: 1, label: 'X'})
+rotation.addBinding(CONFIG, 'rotatez', { min: 0, max: 360, step: 1, label: 'Z'})
+const masker = controlPanel.addFolder({ title: 'Mask', expanded: false })
+masker.addBinding(CONFIG, 'masklower', {
+  label: 'Lower (%)',
+  min: 0,
+  max: 50,
+  step: 1,
+})
+masker.addBinding(CONFIG, 'maskupper', {
+  label: 'Upper (%)',
+  min: 0,
+  max: 50,
+  step: 1,
+})
+
+controlPanel.addBinding(CONFIG, 'backface', { label: 'Backface' })
+controlPanel.addBinding(CONFIG, 'perspective', {
+  label: 'Perspective (px)',
+  min: 50,
+  max: 1500,
+  step: 1,
+})
 controlPanel.addBinding(CONFIG, 'gap', {
   label: 'Gap (%)',
-  min: -0.1,
+  min: 0,
   max: 5,
   step: 0.1,
 })
@@ -111,9 +168,11 @@ const itemSwitch = controlPanel.addBinding(CONFIG, 'items', {
   max: 50,
   step: 1,
 })
+controlPanel.addBinding(CONFIG, 'dark', { label: 'Dark Theme' })
 
 render()
 sync()
 
 controlPanel.on('change', sync)
+// spreader.on('change', render)
 itemSwitch.on('change', render)
