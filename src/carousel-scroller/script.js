52aa0bd9936ee2fa0f0ded591e5d2d603d98e686
jh3y <jh3y@users.noreply.github.com>
2024-05-10 23:06:58 +0100

    feat: check in carousel scroller

--- src/carousel-scroller/script.js
+++ src/carousel-scroller/script.js
@@ -1,4 +1,6 @@
 import { Pane } from 'https://cdn.skypack.dev/tweakpane'
+import gsap from 'https://cdn.skypack.dev/gsap@3.12.0'
+import { ScrollTrigger } from 'https://cdn.skypack.dev/gsap@3.12.0/ScrollTrigger'
 
 const CONFIG = {
   debug: false,
@@ -6,12 +8,13 @@ const CONFIG = {
   // zoom: false,
   backface: false,
   // rotate: false,
+  buff: 2,
   animate: true,
   scroll: true,
   dark: true,
-  masklower: 30,
-  maskupper: 10,
-  perspective: 280,
+  masklower: 0.9,
+  maskupper: 1.8,
+  perspective: 320,
   vertical: false,
   infinite: false,
   items: 16,
@@ -87,6 +90,8 @@ const render = () => {
   setupController()
 }
 
+let tween
+
 const update = () => {
   document.documentElement.dataset.debug = CONFIG.debug
   // document.documentElement.dataset.spread = CONFIG.spread
@@ -103,10 +108,39 @@ const update = () => {
   document.documentElement.style.setProperty('--rotate-z', CONFIG.rotatez)
   document.documentElement.style.setProperty('--mask-lower', CONFIG.masklower)
   document.documentElement.style.setProperty('--mask-upper', CONFIG.maskupper)
+  document.documentElement.style.setProperty('--scroll-ratio', CONFIG.buff)
   document.documentElement.style.setProperty(
     '--perspective',
     CONFIG.perspective
   )
+  // If we're scroll driving, use GSAP
+  if (
+    !CSS.supports('animation-timeline: scroll()') &&
+    CONFIG.scroll &&
+    CONFIG.animate
+  ) {
+    // transform: translate3d(0, 0, var(--radius)) rotateX(calc(var(--rotate-x) * 1deg)) rotateZ(calc(var(--rotate-z) * 1deg)) rotateY(-360deg);
+    if (scroller) scroller[CONFIG.vertical ? 'scrollTop' : 'scrollLeft'] = 0
+    document.documentElement.dataset.gsap = true
+    gsap.registerPlugin(ScrollTrigger)
+    gsap.set(['.carousel'], { animation: 'none', '--rotate': 0 })
+    tween = gsap.to('.carousel', {
+      rotateY: -360,
+      '--rotate': 360,
+      ease: 'none',
+      scrollTrigger: {
+        horizontal: !CONFIG.vertical,
+        scroller: '.controller',
+        scrub: true,
+      },
+    })
+  } else {
+    document.documentElement.dataset.gsap = false
+    gsap.set('.carousel', { clearProps: true })
+    if (tween) tween.kill()
+    ScrollTrigger.killAll()
+    document.querySelector('.carousel').removeAttribute('style')
+  }
 }
 
 const sync = (event) => {
@@ -127,26 +161,45 @@ const controlPanel = new Pane({ title: 'Config', expanded: false })
 // controlPanel.addBinding(CONFIG, 'zoom', { label: 'Zoom' })
 // controlPanel.addBinding(CONFIG, 'rotate', { label: 'Rotate' })
 controlPanel.addBinding(CONFIG, 'animate', { label: 'Animate' })
-const scrolling = controlPanel.addFolder({ title: 'Scrolling' , expanded: false })
+const scrolling = controlPanel.addFolder({
+  title: 'Scrolling',
+  expanded: false,
+})
 scrolling.addBinding(CONFIG, 'scroll', { label: 'Scroll Drive' })
 scrolling.addBinding(CONFIG, 'vertical', { label: 'Vertical' })
 scrolling.addBinding(CONFIG, 'infinite', { label: 'Infinite' })
+scrolling.addBinding(CONFIG, 'buff', {
+  label: 'Ratio',
+  min: 1,
+  max: 10,
+  step: 0.1,
+})
 scrolling.addBinding(CONFIG, 'debug', { label: 'Debug' })
 const rotation = controlPanel.addFolder({ title: 'Rotation', expanded: false })
-rotation.addBinding(CONFIG, 'rotatex', { min: 0, max: 360, step: 1, label: 'X'})
-rotation.addBinding(CONFIG, 'rotatez', { min: 0, max: 360, step: 1, label: 'Z'})
+rotation.addBinding(CONFIG, 'rotatex', {
+  min: 0,
+  max: 360,
+  step: 1,
+  label: 'X',
+})
+rotation.addBinding(CONFIG, 'rotatez', {
+  min: 0,
+  max: 360,
+  step: 1,
+  label: 'Z',
+})
 const masker = controlPanel.addFolder({ title: 'Mask', expanded: false })
 masker.addBinding(CONFIG, 'masklower', {
-  label: 'Lower (%)',
+  label: 'Lower (Item W)',
   min: 0,
-  max: 50,
-  step: 1,
+  max: 5,
+  step: 0.1,
 })
 masker.addBinding(CONFIG, 'maskupper', {
-  label: 'Upper (%)',
+  label: 'Upper (Item W)',
   min: 0,
-  max: 50,
-  step: 1,
+  max: 5,
+  step: 0.1,
 })
 
 controlPanel.addBinding(CONFIG, 'backface', { label: 'Backface' })
@@ -176,3 +229,7 @@ sync()
 controlPanel.on('change', sync)
 // spreader.on('change', render)
 itemSwitch.on('change', render)
+
+if (!CSS.supports('animation-timeline: scroll()')) {
+  gsap.registerPlugin(ScrollTrigger)
+}
