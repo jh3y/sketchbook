dc55b6258b3870d6eb757ebd3c4fe17f818b2f2d
jh3y <jh3y@users.noreply.github.com>
2024-06-08 17:45:30 +0100

    feat: login disclosure

--- src/login-disclosure/script.js
+++ src/login-disclosure/script.js
@@ -1,7 +1,10 @@
 import { Pane } from 'https://cdn.skypack.dev/tweakpane'
+import gsap from 'https://cdn.skypack.dev/gsap@3.12.0'
+import Draggable from 'https://cdn.skypack.dev/gsap@3.12.0/Draggable'
 
 const config = {
-  direction: 'center',
+  inset: 'center',
+  theme: 'system',
 }
 
 const ctrl = new Pane({
@@ -9,13 +12,46 @@ const ctrl = new Pane({
   expanded: true,
 })
 
-ctrl.addBinding(config, 'direction', {
-  label: 'Direction',
+ctrl.addBinding(config, 'inset', {
+  label: 'Position',
   options: {
     Center: 'center',
-    Top: 'top',
-    Right: 'right',
-    Bottom: 'bottom',
-    Left: 'left',
+    Top: 'center span-top',
+    'Top Right': 'span-right span-top',
+    Right: 'span-right center',
+    'Bottom Right': 'span-right span-bottom',
+    Bottom: 'center span-bottom',
+    'Bottom left': 'span-left span-bottom',
+    Left: 'span-left center',
+    'Top Left': 'span-left span-top',
   },
+})
+
+ctrl.addBinding(config, 'theme', {
+  label: 'Theme',
+  options: {
+    System: 'system',
+    Light: 'light',
+    Dark: 'dark',
+  },
+})
+
+const sync = () => {
+  document.documentElement.dataset.theme = config.theme
+  document.documentElement.style.setProperty('--inset', config.inset)
+}
+
+sync()
+
+const handle = (event) => {
+  if (!document.startViewTransition) return sync()
+  document.startViewTransition(() => sync())
+}
+
+ctrl.on('change', handle)
+
+gsap.registerPlugin(Draggable)
+
+Draggable.create('.sign-in', {
+  type: 'top,left',
 })
