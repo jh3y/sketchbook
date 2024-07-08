21b365c0ee7276532034e8c6e4303f9aea972673
jh3y <jh3y@users.noreply.github.com>
2024-07-08 20:51:05 +0100

    feat: check in cursor trail changes

--- src/icon-cursor-trail/script.js
+++ src/icon-cursor-trail/script.js
@@ -1,4 +1,26 @@
+import { Pane } from 'https://cdn.skypack.dev/tweakpane'
 import { gsap } from 'https://cdn.skypack.dev/gsap@3.12.0'
+
+const config = {
+  distance: 50,
+  glow: false,
+}
+
+const ctrl = new Pane({
+  title: 'Config',
+  expanded: true,
+})
+
+ctrl.addBinding(config, 'distance', {
+  min: 0,
+  max: 200,
+  step: 1,
+  label: 'Buffer (px)',
+})
+
+ctrl.addBinding(config, 'glow', {
+  label: 'Glow',
+})
 
 // Get the serialized SVG string
 const canvas = document.querySelector('canvas')
@@ -26,6 +48,7 @@ img.onload = function () {
 // Set the image source to the Data URI
 img.src = url
 let parts = []
+let glows = []
 
 const render = () => {
   ctx.clearRect(
@@ -34,12 +57,25 @@ const render = () => {
     window.innerWidth * window.devicePixelRatio,
     window.innerHeight * window.devicePixelRatio
   )
+
+  for (const glow of glows) {
+    ctx.beginPath()
+    // Draw the circle
+    // arc(x, y, radius, startAngle, endAngle, anticlockwise)
+    ctx.arc(glow.x, glow.y, glow.size * glow.scale, 0, Math.PI * 2)
+    // Optional: set fill color
+    ctx.fillStyle = 'hsl(265 90% 80% / 0.2)'
+    // Fill the circle
+    ctx.fill()
+  }
+
   for (const part of parts) {
     ctx.save()
     ctx.globalAlpha = part.alpha
-    ctx.filter = `hue-rotate(${part.sy < 0 ? '150' : '0'}deg) brightness(${
-      part.b
-    })`
+    // ctx.filter = `hue-rotate(${part.sy < 0 ? '150' : '0'}deg) brightness(${
+    //   part.sy < 0 ? part.b - 0.5 : part.b
+    // })`
+    ctx.filter = `brightness(${part.sy < 0 ? part.b + 0.5 : part.b})`
 
     // const x = part.x - part.size * 0.5
     // const y = part.y - part.size * 0.5
@@ -65,36 +101,66 @@ const render = () => {
   }
 }
 
+let distance = 0
+let lastPoint = []
 const paint = ({ x, y }) => {
-  const newPart = {
-    id: Date.now(),
-    x,
-    y,
-    sy: Math.random() > 0.5 ? 1 : -1,
-    b: gsap.utils.random(0.5, 1.5),
-    r: gsap.utils.random(0, 359, 1),
-    hue: gsap.utils.random(0, 359, 1),
-    size: gsap.utils.random(10, 40, 1),
-    scale: 1,
-    alpha: 1,
+  const last = lastPoint
+  const dist = Math.hypot(x - last[0], y - last[1])
+  lastPoint = [x, y]
+
+  if (!Number.isNaN(dist)) distance += dist
+
+  if (config.glow) {
+    const glow = {
+      id: `glow--${Date.now()}`,
+      size: 30,
+      scale: 1,
+      x,
+      y,
+    }
+    gsap.to(glow, {
+      onComplete: () => {
+        glows = glows.filter((g) => g.id !== glow.id)
+      },
+      duration: 0.2,
+      scale: 0,
+    })
+    glows.push(glow)
   }
-  const spin = gsap.to(newPart, {
-    sy: newPart.sy < 0 ? 1 : -1,
-    duration: gsap.utils.random(0.1, 0.5),
-    repeat: gsap.utils.random(0, 10, 1),
-  })
-  gsap.to(newPart, {
-    onComplete: () => {
-      spin.kill()
-      parts = parts.filter((p) => p.id !== newPart.id)
-    },
-    duration: gsap.utils.random(0.5, 2.5),
-    r: newPart.r + gsap.utils.random(-45, 45, 1),
-    y: y + gsap.utils.random(50, 350, 1),
-    alpha: 0,
-    scale: 0,
-  })
-  parts.push(newPart)
+
+  if (distance >= config.distance) {
+    distance = 0
+
+    const newPart = {
+      id: Date.now(),
+      x,
+      y,
+      sy: Math.random() > 0.5 ? 1 : -1,
+      b: gsap.utils.random(0.5, 1.5),
+      r: gsap.utils.random(0, 359, 1),
+      hue: gsap.utils.random(0, 359, 1),
+      size: gsap.utils.random(10, 40, 1),
+      scale: 1,
+      alpha: 1,
+    }
+    const spin = gsap.to(newPart, {
+      sy: newPart.sy < 0 ? 1 : -1,
+      duration: gsap.utils.random(0.1, 0.5),
+      repeat: gsap.utils.random(0, 10, 1),
+    })
+    gsap.to(newPart, {
+      onComplete: () => {
+        spin.kill()
+        parts = parts.filter((p) => p.id !== newPart.id)
+      },
+      duration: gsap.utils.random(0.5, 2.5),
+      r: newPart.r + gsap.utils.random(-45, 45, 1),
+      y: y + gsap.utils.random(50, 350, 1),
+      alpha: 0,
+      scale: 0,
+    })
+    parts.push(newPart)
+  }
 }
 document.body.addEventListener('pointermove', paint)
 gsap.ticker.add(render)
