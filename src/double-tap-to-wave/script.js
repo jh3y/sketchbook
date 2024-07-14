10c76ea4a6a4b1973390c67d8115c612ca18a0d3
jh3y <jh3y@users.noreply.github.com>
2024-07-14 01:45:56 +0100

    feat chcek in

--- src/double-tap-to-wave/script.js
+++ src/double-tap-to-wave/script.js
@@ -1,19 +1,37 @@
 import gsap from 'gsap'
 import { Pane } from 'https://cdn.skypack.dev/tweakpane'
 
-// Code for Wave: &#128075;
-
 const cnv = document.querySelector('canvas')
+cnv.showPopover()
 const ctx = cnv.getContext('2d')
 cnv.width = ctx.width = cnv.offsetWidth
 cnv.height = ctx.height = cnv.offsetHeight
 
-
 const hey = ({ x, y }) => {
-  console.info({ x, y })
   ctx.font = '112px sans-serif'
   // How to render unicode string such as emoji on <canvas>
-  ctx.fillText(String.fromCodePoint('0x1F44B'), x - 56, y)
+  // ctx.fillText(String.fromCodePoint('0x1F44B'), x - 56, y)
+  ctx.fillText('👋', x - 56, y)
 }
 
 document.body.addEventListener('dblclick', hey)
+
+// Radial menu
+const onActive = () => console.info('activated')
+const useContextHold = () => {
+  // set up
+  const close = () => {
+    window.removeEventListener('pointerup', close)
+  }
+  const open = (event) => {
+    if (event.buttons === 2) {
+      // Enter context mode
+
+      window.addEventListener('pointerup', close)
+    }
+  }
+  window.addEventListener('contextmenu', (e) => e.preventDefault())
+  window.addEventListener('pointerdown', open)
+}
+
+useContextHold({ onActive })
