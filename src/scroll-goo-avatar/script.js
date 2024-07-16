4ea2ac2586871a23decb6b5e3f90649755db16f3
jh3y <jh3y@users.noreply.github.com>
2024-07-16 21:33:35 +0100

    feat: check in

---
+++ src/scroll-goo-avatar/script.js
@@ -0,0 +1,12 @@
+// import { Pane } from 'https://cdn.skypack.dev/tweakpane'
+
+// const config = {
+//   debug: false,
+// }
+
+// const ctrl = new Pane({
+//   title: 'Config',
+//   expanded: true,
+// })
+
+// ctrl.addBinding(config, 'debug', { label: 'Debug' })
