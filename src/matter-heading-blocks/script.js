63852909c3edd1b8966fec3938bcc387c4f5f0fc
jh3y <jh3y@users.noreply.github.com>
2024-09-07 01:52:30 +0100

    feat: check in responsive attempt and gravity config

--- src/matter-heading-blocks/script.js
+++ src/matter-heading-blocks/script.js
@@ -4,11 +4,38 @@ import Matter from 'https://cdn.skypack.dev/matter-js'
 import Splitting from 'https://cdn.skypack.dev/splitting'
 
 Splitting()
-
 const config = {
   debug: false,
   theme: 'system',
+  x: 0,
+  y: 0.25,
+  scale: 0.01,
+  blockCount: 35,
 }
+const DPR = window.devicePixelRatio
+const thickness = window.innerWidth
+
+const engine = Matter.Engine.create({
+  render: {
+    options: {
+      pixelRatio: DPR,
+    },
+  },
+})
+engine.gravity.x = config.x
+engine.gravity.y = config.y
+engine.gravity.scale = config.scale
+
+const wordBodies = []
+
+const blockBodies = []
+const blockRenderers = []
+const blockSize = 60
+const blockCount = config.blockCount
+// Kick off the render...
+let frame = 0
+const buffer = 12
+const size = 60 * DPR
 
 const ctrl = new Pane({
   title: 'Config',
@@ -18,6 +45,9 @@ const ctrl = new Pane({
 const update = () => {
   document.documentElement.dataset.debug = config.debug
   document.documentElement.dataset.theme = config.theme
+  engine.gravity.x = config.x
+  engine.gravity.y = config.y
+  engine.gravity.scale = config.scale
 }
 
 const sync = (event) => {
@@ -45,123 +75,115 @@ ctrl.addBinding(config, 'theme', {
 ctrl.on('change', sync)
 update()
 
-const DPR = window.devicePixelRatio
-
-console.info({ DPR })
-
-// Set up the physics engine...
-const engine = Matter.Engine.create({
-  render: {
-    options: {
-      pixelRatio: DPR,
-    },
-  },
-})
-engine.gravity.x = 0
-engine.gravity.y = 0.25
-engine.gravity.scale = 0.01
-
-const wordElements = document.querySelectorAll('.word')
-
-const wordBodies = []
-
-const blocks = new Array(20).fill().map((e) =>
-  Object.assign(document.createElement('div'), {
-    className: 'block',
-  })
-)
-
-const blockBodies = []
-const blockRenderers = []
-const blockSize = 60
-for (let i = 0; i < 35; i++) {
-  // header.appendChild(block)
-  // block.style.setProperty('--l', Math.random() * 0.5)
-  // block.style.setProperty('--t', Math.random() * 0.2)
-  // const bounds = block.getBoundingClientRect()
-  const newBlock = {
-    w: blockSize,
-    h: blockSize,
-    fill: `hsl(${Math.random() * 359} 80% 80%)`,
-    body: Matter.Bodies.rectangle(
+const createBlocks = () => {
+  blockBodies.length = 0
+  for (let i = 0; i < config.blockCount; i++) {
+    const newBlock = Matter.Bodies.rectangle(
       Math.random() * (window.innerWidth * 0.5),
       Math.random() * (window.innerHeight * -0.2) - blockSize,
       blockSize,
-      blockSize
-    ),
-    // elem: block,
-    render() {
-      const { x, y } = this.body.position
-      // this.elem.style.setProperty('--y', y)
-      // this.elem.style.setProperty('--x', x)
-      // this.elem.style.setProperty('--r', this.body.angle)
-      // this.elem.style.top = `${y - this.h / 2}px`
-      // this.elem.style.left = `${x - this.w / 2}px`
-      // this.elem.style.transform = `rotate(${this.body.angle}rad)`
-    },
+      blockSize,
+      {
+        w: blockSize,
+        h: blockSize,
+        fill: `hsl(${Math.random() * 359} 80% 80%)`,
+      }
+    )
+    blockBodies.push(newBlock)
   }
-
-  blockBodies.push(newBlock.body)
-  blockRenderers.push(newBlock)
 }
+createBlocks()
 
 for (const elem of document.querySelectorAll('.word')) {
-  // Create new bodies and add them to the engine so they can be rendered.
-  // Give them random placement though
   const bounds = elem.getBoundingClientRect()
 
-  const newBody = {
-    w: bounds.width,
-    h: bounds.height,
-    body: Matter.Bodies.rectangle(
-      bounds.x + bounds.width * 0.5,
-      bounds.y + bounds.height * 0.5 + window.scrollY,
-      bounds.width,
-      bounds.height,
-      {
-        isStatic: true,
-      }
-    ),
-  }
-
-  wordBodies.push(newBody.body)
-  // wordRenderers.push(newBody)
+  const newBody = Matter.Bodies.rectangle(
+    bounds.x + bounds.width * 0.5,
+    bounds.y + bounds.height * 0.5 + window.scrollY,
+    bounds.width,
+    bounds.height,
+    {
+      elem,
+      isStatic: true,
+    }
+  )
+  wordBodies.push(newBody)
 }
+
+const syncWords = () => {
+  for (const body of wordBodies) {
+    const bounds = body.elem.getBoundingClientRect()
+    Matter.Body.setPosition(body, {
+      x: bounds.x + bounds.width * 0.5,
+      y: bounds.y + bounds.height * 0.5 + window.scrollY,
+    })
+    Matter.Body.set(body, {
+      width: bounds.width,
+      height: bounds.height,
+    })
+  }
+}
+syncWords()
 
 const ceiling = Matter.Bodies.rectangle(
   window.innerWidth * 0.5,
-  window.innerHeight * -0.5,
-  window.innerWidth,
-  10,
+  window.innerHeight * -1 - thickness * 0.5,
+  window.innerWidth * 10,
+  thickness,
   {
     isStatic: true,
   }
 )
 const ground = Matter.Bodies.rectangle(
   window.innerWidth * 0.5,
-  window.innerHeight,
-  window.innerWidth,
-  10,
+  window.innerHeight + thickness * 0.5,
+  window.innerWidth * 10,
+  thickness,
   {
     isStatic: true,
   }
 )
 const wallRight = Matter.Bodies.rectangle(
-  window.innerWidth,
+  window.innerWidth + thickness * 0.5,
   0,
-  10,
-  window.innerHeight * 2,
+  thickness,
+  window.innerHeight * 10,
   {
     isStatic: true,
   }
 )
-const wallLeft = Matter.Bodies.rectangle(-10, 0, 10, window.innerHeight * 2, {
-  isStatic: true,
-})
+const wallLeft = Matter.Bodies.rectangle(
+  thickness * -0.5,
+  0,
+  thickness,
+  window.innerHeight * 10,
+  {
+    isStatic: true,
+  }
+)
+
+const syncWalls = () => {
+  Matter.Body.setPosition(ground, {
+    x: window.innerWidth * 0.5,
+    y: window.innerHeight + thickness * 0.5,
+  })
+
+  Matter.Body.setPosition(ceiling, {
+    x: window.innerWidth * 0.5,
+    y: window.innerHeight * -1 - thickness * 0.5,
+  })
+  Matter.Body.setPosition(wallRight, {
+    x: window.innerWidth + thickness * 0.5,
+    y: 0,
+  })
+}
+
+// Add the dragging
 const mouseConstraint = Matter.MouseConstraint.create(engine, {
   element: document.documentElement,
 })
 
+// Remove the scroll interception so we can scroll past
 mouseConstraint.mouse.element.removeEventListener(
   'wheel',
   mouseConstraint.mouse.mousewheel
@@ -171,6 +193,18 @@ mouseConstraint.mouse.element.removeEventListener(
   mouseConstraint.mouse.mousewheel
 )
 
+const resetBlocks = () => {
+  blockBodies.map((b) => {
+    Matter.Composite.remove(engine.world, b)
+    Matter.Body.setPosition(b, {
+      x: Math.random() * (window.innerWidth * 0.5),
+      y: Math.random() * (window.innerHeight * -0.2) - blockSize,
+    })
+    b.__added = false
+  })
+}
+
+// Add everything into the world
 Matter.Composite.add(engine.world, [
   ceiling,
   ground,
@@ -185,10 +219,6 @@ const ctx = canvas.getContext('2d')
 canvas.width = canvas.offsetWidth * DPR
 canvas.height = canvas.offsetHeight * DPR
 
-// Kick off the render...
-let frame = 0
-const buffer = 12
-const size = 60 * DPR
 const render = () => {
   frame++
   const newBody = blockBodies.filter((body) => !body.__added)[0]
@@ -198,26 +228,15 @@ const render = () => {
   }
   // Rendera all the bodies in here
   ctx.clearRect(0, 0, canvas.width, canvas.height)
-  for (const entity of blockRenderers) {
-    entity.render()
-
+  for (const entity of blockBodies) {
     ctx.save()
-
-    ctx.translate(entity.body.position.x * DPR, entity.body.position.y * DPR)
-    ctx.rotate(entity.body.angle)
+    ctx.translate(entity.position.x * DPR, entity.position.y * DPR)
+    ctx.rotate(entity.angle)
     ctx.fillStyle = entity.fill || 'black'
     ctx.fillRect(size * -0.5, size * -0.5, size, size)
     ctx.restore()
   }
-  // ctx.save()
-  // ctx.translate(ground.position.x * DPR, ground.position.y * DPR)
-  // ctx.fillRect(
-  //   window.innerWidth * DPR * -0.5,
-  //   10 * DPR * -0.5,
-  //   window.innerWidth * DPR,
-  //   10 * DPR
-  // )
-  // ctx.restore()
+
   Matter.Engine.update(engine)
 }
 
@@ -225,13 +244,68 @@ setTimeout(() => {
   document.documentElement.dataset.active = true
   gsap.ticker.add(render)
 }, 0)
+ctrl
+  .addBinding(config, 'blockCount', {
+    min: 1,
+    max: 100,
+    step: 1,
+    label: 'Blocks',
+  })
+  .on('change', () => {
+    blockBodies.map((b) => {
+      Matter.Composite.remove(engine.world, b)
+      Matter.Body.setPosition(b, {
+        x: Math.random() * (window.innerWidth * 0.5),
+        y: Math.random() * (window.innerHeight * -0.2) - blockSize,
+      })
+      b.__added = false
+    })
+    blockBodies.length = 0
+    createBlocks()
+  })
+const gravity = ctrl.addFolder({ title: 'Gravity' })
+gravity.addBinding(config, 'x', {
+  label: 'x',
+  min: -1,
+  max: 1,
+  step: 0.01,
+})
+gravity.addBinding(config, 'y', {
+  label: 'y',
+  min: -1,
+  max: 1,
+  step: 0.001,
+})
+gravity.addBinding(config, 'scale', {
+  label: 'scale',
+  min: -1,
+  max: 1,
+  step: 0.01,
+})
+gravity
+  .addButton({
+    title: 'Reset',
+  })
+  .on('click', () => {
+    config.x = 0
+    config.y = 0.25
+    config.scale = 0.01
+    ctrl.refresh()
+    update()
+  })
+ctrl
+  .addButton({
+    title: 'Drop',
+  })
+  .on('click', resetBlocks)
 
-const reset = () => {
-  for (const body of blockBodies) {
-    body.__added = false
-    body.position.x = Math.random() * (window.innerWidth * 0.5)
-    body.position.y = Math.random() * (window.innerHeight * 0.2)
-  }
+const adapt = () => {
+  // Here you need to update all the things...
+  canvas.width = canvas.offsetWidth * DPR
+  canvas.height = canvas.offsetHeight * DPR
+
+  syncWalls()
+  syncWords()
 }
 
-window.addEventListener('resize', reset)
+window.addEventListener('resize', adapt)
