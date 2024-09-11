be667cc2e1fc9c4176d2d7779e672ae0e5122e73
jh3y <jh3y@users.noreply.github.com>
2024-09-11 01:46:37 +0100

    feat: scroll to decrypt

--- src/scroll-decrypt/script.js
+++ src/scroll-decrypt/script.js
@@ -1,10 +1,16 @@
 import { Pane } from 'https://cdn.skypack.dev/tweakpane'
+import Splitting from 'https://cdn.skypack.dev/splitting'
 
-const heading = document.querySelector('h1')
+const heading = document.querySelector('.encrypted')
 
 const config = {
-  text: 'Hello World!',
+  debug: false,
+  title: 'Pretty cool',
+  sub: 'Translate clipped text on scroll',
   theme: 'system',
+  track: 10,
+  start: 1,
+  end: -0.2,
 }
 
 const ctrl = new Pane({
@@ -34,22 +40,40 @@ const escapeHTML = (str) =>
       }[char])
   )
 
+const getWords = (txt) => {
+  const hold = Object.assign(document.createElement('div'), {
+    innerHTML: Splitting.html({ content: txt, whitespace: true }),
+  })
+
+  const chars = hold.querySelectorAll('.char, .whitespace')
+  hold.firstChild.setAttribute('aria-hidden', 'true')
+  for (let c = 0; c < chars.length; c++) {
+    const char = chars[c]
+    char.dataset.char = char.innerHTML
+    char.innerHTML = `<span>${char.innerHTML}${escapeHTML(
+      randomString(config.track)
+    )}</span>`
+  }
+
+  return hold.innerHTML
+}
+
 const update = () => {
   heading.innerHTML = `
-    <span class="sr-only">${config.text}</span>
-    ${config.text
-      .split('')
-      .map(
-        (c, i) =>
-          `<span style="--i: ${i};" data-char="${c}"><span>${
-            c.trim() === '' ? '<span>&nbsp;</span>' : c
-          }${escapeHTML(randomString(10))}</span><span>${
-            c.trim() === '' ? '0' : c
-          }</span></span>`
-      )
-      .join('')}
+    <h1>
+      <span class="sr-only">${config.title}</span>
+      ${getWords(config.title)}
+    </h1>
+    <p>
+      <span class="sr-only">${config.sub}</span>
+      ${getWords(config.sub)}
+    </p>
   `
   document.documentElement.dataset.theme = config.theme
+  document.documentElement.dataset.debug = config.debug
+  document.documentElement.style.setProperty('--track', config.track)
+  document.documentElement.style.setProperty('--start', config.start)
+  document.documentElement.style.setProperty('--end', config.end)
 }
 
 const sync = (event) => {
@@ -61,8 +85,34 @@ const sync = (event) => {
   document.startViewTransition(() => update())
 }
 
-ctrl.addBinding(config, 'text', {
+ctrl.addBinding(config, 'title', {
   label: 'Text',
+})
+ctrl.addBinding(config, 'sub', {
+  label: 'Sub',
+})
+ctrl.addBinding(config, 'track', {
+  min: 2,
+  max: 50,
+  step: 1,
+  label: 'Chars',
+})
+const range = ctrl.addFolder({ title: 'Range Step' })
+range.addBinding(config, 'start', {
+  min: -10,
+  max: 10,
+  step: 0.01,
+  label: 'Start',
+})
+range.addBinding(config, 'end', {
+  min: -10,
+  max: 10,
+  step: 0.01,
+  label: 'End',
+})
+
+ctrl.addBinding(config, 'debug', {
+  label: 'Debug',
 })
 
 ctrl.addBinding(config, 'theme', {
