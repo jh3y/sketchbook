a74df181c41a6a48c0d5ea8c0921644bd9aa5046
jh3y <jh3y@users.noreply.github.com>
2024-09-14 13:09:36 +0100

    feat: check in

--- src/matter-heading-blocks/script.js
+++ src/matter-heading-blocks/script.js
@@ -3,6 +3,33 @@ import gsap from 'https://cdn.skypack.dev/gsap@3.12.0'
 import Matter from 'https://cdn.skypack.dev/matter-js'
 import Splitting from 'https://cdn.skypack.dev/splitting'
 
+// Icons
+const icons = {
+  twitter:
+    '<svg fill="currentColor" class="w-5 h-5" viewBox="0 0 24 24" role="img"><title>Twitter icon</title><path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z"></path></svg>',
+}
+let loaders = []
+let images = []
+for (const icon of Object.values(icons)) {
+  const img = new Image()
+
+  // console.info({ icon })
+  // console.info(el.firstElementChild)
+  const blob = new Blob([icon], {
+    type: 'image/svg+xml;charset=utf-8',
+  })
+
+  console.info({ icon })
+  const url = URL.createObjectURL(blob)
+  img.onload = function () {
+    URL.revokeObjectURL(url)
+  }
+  // loaders.push(img.onload)
+  images.push(img)
+  img.src = url
+}
+// Promise.allSettled(loaders).then(() => console.info('all loaded'))
+
 Splitting()
 const config = {
   debug: false,
@@ -84,6 +111,7 @@ const createBlocks = () => {
       blockSize,
       blockSize,
       {
+        angle: gsap.utils.random(0, 359),
         w: blockSize,
         h: blockSize,
         fill: `hsl(${Math.random() * 359} 80% 80%)`,
@@ -240,10 +268,10 @@ const render = () => {
   Matter.Engine.update(engine)
 }
 
-setTimeout(() => {
-  document.documentElement.dataset.active = true
-  gsap.ticker.add(render)
-}, 0)
+setTimeout(() => {}, 0)
+document.documentElement.dataset.active = true
+gsap.ticker.add(render)
+
 ctrl
   .addBinding(config, 'blockCount', {
     min: 1,
