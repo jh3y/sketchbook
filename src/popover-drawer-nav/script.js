adac8b781d7e97ac7de5b20687ef3eef0d5b51d6
jh3y <jh3y@users.noreply.github.com>
2024-10-18 23:47:54 +0100

    feat: check in place check

--- src/popover-drawer-nav/script.js
+++ src/popover-drawer-nav/script.js
@@ -1,15 +1,21 @@
 import { Pane } from 'https://cdn.skypack.dev/tweakpane'
 import Splitting from 'https://cdn.skypack.dev/splitting'
+import gsap from 'https://cdn.skypack.dev/gsap@3.12.0'
+import { Draggable } from 'https://cdn.skypack.dev/gsap@3.12.0/Draggable'
+import InertiaPlugin from 'gsap/InertiaPlugin'
+gsap.registerPlugin(Draggable, InertiaPlugin)
 
 Splitting()
 
 const popover = document.querySelector('#nav')
 const themer = document.querySelector('.theme-toggle')
 const debug = document.querySelector('.debug-toggle')
+const placer = document.querySelector('.placement-toggle')
 
 let deb
 document.documentElement.dataset.resizing = false
 document.documentElement.dataset.theme = 'system'
+document.documentElement.dataset.placement = 'top'
 
 window.addEventListener('resize', () => {
   if (popover.matches(':popover-open')) popover.hidePopover()
@@ -20,13 +26,18 @@ window.addEventListener('resize', () => {
   }, 200)
 })
 
+placer.addEventListener('click', () => {
+  document.documentElement.dataset.placement =
+    document.documentElement.dataset.placement === 'top' ? 'bottom' : 'top'
+})
+
 const config = {
   theme: 'system',
   debug: false,
 }
 
 let themeIndex = 0
-let themeOptions = ['system', 'light', 'dark']
+const themeOptions = ['system', 'light', 'dark']
 const switchTheme = () => {
   themeIndex += 1
   config.theme = themeOptions[themeIndex % 3]
@@ -51,17 +62,83 @@ const sync = (event) => {
   document.startViewTransition(() => update())
 }
 
-// ctrl.addBinding(config, 'theme', {
-//   label: 'Theme',
-//   options: {
-//     System: 'system',
-//     Light: 'light',
-//     Dark: 'dark',
-//   },
-// })
-// ctrl.addBinding(config, 'debug', {
-//   label: 'Debug',
-// })
-
-// ctrl.on('change', sync)
 update()
+let dragger
+const hider = document.querySelector('.hider')
+hider.removeAttribute('popovertargetaction')
+const PROXY = document.createElement('div')
+const threshold = () => window.innerHeight * 0.25
+const reset = () => {
+  dragger[0].update()
+  gsap.set(PROXY, { clearProps: 'y' })
+  document.documentElement.dataset.complete = false
+  document.documentElement.style.setProperty('--complete', 0)
+  popover.style.setProperty('--ty', 0)
+}
+let distance
+dragger = Draggable.create(PROXY, {
+  type: 'y',
+  inertia: true,
+  allowContextMenu: true,
+  trigger: '.hider',
+  onDragStart: () => {
+    reset()
+    document.documentElement.dataset.dragging = true
+  },
+  onPressInit: () => {
+    // at this point, track the size of the viewport and drawer
+    distance = popover.offsetHeight
+    popover.style.setProperty('--distance', distance)
+    reset()
+  },
+  onDrag: function () {
+    document.documentElement.style.setProperty('--complete', this.y / distance)
+    popover.style.setProperty('--ty', this.y)
+  },
+  onRelease: () => {
+    document.documentElement.dataset.dragging = false
+  },
+  onThrowUpdate: () => {
+    document.documentElement.dataset.throwing = true
+  },
+  onThrowComplete: function () {
+    document.documentElement.dataset.throwing = false
+    if (this.y > threshold() || this.endY > threshold()) {
+      popover.hidePopover()
+    } else {
+      reset()
+    }
+  },
+  onDragEnd: function () {
+    // this is your inertia ending here.
+    popover.style.setProperty('--ty', this.endY)
+    document.documentElement.style.setProperty(
+      '--complete',
+      this.endY / distance
+    )
+
+    const transitions = popover.getAnimations()
+    console.info({ transitions })
+    if (this.endY / distance) document.documentElement.dataset.complete = true
+    const transition = transitions[0]
+    const end = () => {
+      document.documentElement.dataset.throwing = false
+      if (this.y > threshold() || this.endY > threshold()) {
+        popover.hidePopover()
+      } else {
+        reset()
+      }
+    }
+    if (transition && this.isThrowing) {
+      transition.finished
+        .then(() => {
+          end()
+        })
+        .catch((err) => console.info('wtu'))
+    } else {
+      end()
+    }
+  },
+})
+
+popover.addEventListener('toggle', reset)
