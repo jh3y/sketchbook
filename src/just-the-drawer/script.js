d5cbf233ff73ef72a00be908a43aa0a0062788c9
jh3y <jh3y@users.noreply.github.com>
2024-10-21 18:08:32 +0100

    feat: check in drawer

--- src/just-the-drawer/script.js
+++ src/just-the-drawer/script.js
@@ -6,6 +6,7 @@ const config = {
 
 const drawer = document.querySelector('.drawer')
 const scroller = drawer.querySelector('.drawer__scroller')
+const dragger = drawer.querySelector('.drawer__drag')
 
 const ctrl = new Pane({
   title: 'Config',
@@ -49,3 +50,57 @@ scroller.addEventListener('scrollsnapchange', () => {
 drawer.addEventListener('toggle', (event) => {
   if (event.newState === 'closed') drawer.dataset.snapped = false
 })
+
+const attachDrag = (element) => {
+  let startY = 0
+  let drag = 0
+  let scrollStart
+
+  const reset = () => {
+    startY = drag = 0
+    const top = scroller.scrollTop < scrollStart * 0.5 ? 0 : scrollStart
+
+    const handleScroll = () => {
+      if (scroller.scrollTop === top) {
+        document.documentElement.dataset.dragging = false
+        scroller.removeEventListener('scroll', handleScroll)
+      }
+    }
+    scroller.addEventListener('scroll', handleScroll)
+
+    scroller.scrollTo({
+      top,
+      behavior: 'smooth',
+    })
+    handleScroll()
+  }
+
+  const handle = ({ y }) => {
+    drag = y - startY
+    scroller.scrollTo({
+      top: scrollStart - drag,
+      behavior: 'instant',
+    })
+  }
+  const teardown = (event) => {
+    if (event.target.tagName !== 'BUTTON') {
+      reset()
+    }
+    document.removeEventListener('mousemove', handle)
+    document.removeEventListener('mouseup', teardown)
+  }
+  const activate = ({ y }) => {
+    startY = y
+    scrollStart = scroller.scrollTop
+    document.documentElement.dataset.dragging = true
+    document.addEventListener('mousemove', handle)
+    document.addEventListener('mouseup', teardown)
+  }
+  element.addEventListener('click', (event) => {
+    if (drag > 5) event.preventDefault()
+    reset()
+  })
+  element.addEventListener('mousedown', activate)
+}
+
+attachDrag(document.querySelector('.drawer__content'))
